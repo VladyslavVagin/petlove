ded2267edbb20006d13a8cfc2e034f15d96e86a5
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-05-01 11:15:40 +0200

    Filters with any two parameters
    Working

--- src/redux/notices/operations.js
+++ src/redux/notices/operations.js
@@ -65,6 +65,7 @@ export const fetchNotices = createAsyncThunk("notices/fetchAll",
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
   try {
+    // ================================ IF ONLY ONE PARAMETER RECEIVED =============================================================================================
     if(!keyword && !category && !species && !locationId && !radioSearch && !sex) {
       const response = await axios.get(`/notices?page=${page}`);
       filteredData = response.data;
@@ -106,6 +107,145 @@ export const fetchNotices = createAsyncThunk("notices/fetchAll",
         return filteredData;
       }
     }
+    // ========================================================================================================================================
+    // ====================  IF ONLY 2 (TWO) PARAMETERS ======================================================================================
+    else if (keyword && category && !species && !locationId && !radioSearch && !sex) {
+      const response = await axios.get(`/notices?page=${page}&keyword=${keyword}&category=${category}`);
+      filteredData = response.data;
+    } else if (keyword && !category && species && !locationId && !radioSearch && !sex) {
+      const response = await axios.get(`/notices?page=${page}&keyword=${keyword}&species=${species}`);
+      filteredData = response.data;
+    } else if (keyword && !category && !species && locationId && !radioSearch && !sex) {
+      const response = await axios.get(`/notices?page=${page}&keyword=${keyword}&locationId=${locationId}`);
+      filteredData = response.data;
+    } else if (keyword && !category && !species && !locationId && radioSearch && !sex) {
+      if(radioSearch === "Cheap") {
+        const response = await axios.get(`/notices?page=${page}&keyword=${keyword}&byPrice=true`);
+        filteredData = response.data;
+        return filteredData;
+      } else if (radioSearch === "Expensive") {
+        const response = await axios.get(`/notices?page=${page}&keyword=${keyword}&byPrice=false`);
+        filteredData = response.data;
+        return filteredData;
+      } else if (radioSearch === "Popular") {
+        const response = await axios.get(`/notices?page=${page}&keyword=${keyword}&byPopularity=false`);
+        filteredData = response.data;
+        return filteredData;
+      } else  if (radioSearch === "Unpopular") {
+        const response = await axios.get(`/notices?page=${page}&keyword=${keyword}&byPopularity=true`);
+        filteredData = response.data;
+        return filteredData;
+      }
+    } else if (keyword && !category && !species && !locationId && !radioSearch && sex) {
+      const response = await axios.get(`/notices?&limit=2000&keyword=${keyword}`);
+      let filteredResults = response.data.results.filter(item => item.sex === sex);
+      const totalPages = Math.ceil(filteredResults?.length / 6);
+      const displayedObjects = filteredResults.slice(startIndex, endIndex);
+      const sendData = {results: displayedObjects, totalPages: totalPages};
+      return filteredData = sendData;
+    } else if (!keyword && category && species && !locationId && !radioSearch && !sex) {
+      const response = await axios.get(`/notices?page=${page}&category=${category}&species=${species}`);
+      filteredData = response.data;
+    } else if (!keyword && category && !species && locationId && !radioSearch && !sex) {
+      const response = await axios.get(`/notices?page=${page}&category=${category}&locationId=${locationId}`);
+      filteredData = response.data;
+    } else if (!keyword && category && !species && !locationId && !radioSearch && sex) {
+      const response = await axios.get(`/notices?&limit=2000&category=${category}`);
+      let filteredResults = response.data.results.filter(item => item.sex === sex);
+      const totalPages = Math.ceil(filteredResults?.length / 6);
+      const displayedObjects = filteredResults.slice(startIndex, endIndex);
+      const sendData = {results: displayedObjects, totalPages: totalPages};
+      return filteredData = sendData;
+    } else if (!keyword && category && !species && !locationId && radioSearch && !sex) {
+      if(radioSearch === "Cheap") {
+        const response = await axios.get(`/notices?page=${page}&category=${category}&byPrice=true`);
+        filteredData = response.data;
+        return filteredData;
+      } else if (radioSearch === "Expensive") {
+        const response = await axios.get(`/notices?page=${page}&category=${category}&byPrice=false`);
+        filteredData = response.data;
+        return filteredData;
+      } else if (radioSearch === "Popular") {
+        const response = await axios.get(`/notices?page=${page}&category=${category}&byPopularity=false`);
+        filteredData = response.data;
+        return filteredData;
+      } else  if (radioSearch === "Unpopular") {
+        const response = await axios.get(`/notices?page=${page}&category=${category}&byPopularity=true`);
+        filteredData = response.data;
+        return filteredData;
+      }
+    } else if (!keyword && !category && species && locationId && !radioSearch && !sex) {
+      const response = await axios.get(`/notices?page=${page}&species=${species}&locationId=${locationId}`);
+      filteredData = response.data;
+    } else if (!keyword && !category && species && !locationId && !radioSearch && sex) {
+      const response = await axios.get(`/notices?&limit=2000&species=${species}`);
+      let filteredResults = response.data.results.filter(item => item.sex === sex);
+      const totalPages = Math.ceil(filteredResults?.length / 6);
+      const displayedObjects = filteredResults.slice(startIndex, endIndex);
+      const sendData = {results: displayedObjects, totalPages: totalPages};
+      return filteredData = sendData;
+    } else if (!keyword && !category && species && !locationId && radioSearch && !sex) {
+      if(radioSearch === "Cheap") {
+        const response = await axios.get(`/notices?page=${page}&species=${species}&byPrice=true`);
+        filteredData = response.data;
+        return filteredData;
+      } else if (radioSearch === "Expensive") {
+        const response = await axios.get(`/notices?page=${page}&species=${species}&byPrice=false`);
+        filteredData = response.data;
+        return filteredData;
+      } else if (radioSearch === "Popular") {
+        const response = await axios.get(`/notices?page=${page}&species=${species}&byPopularity=false`);
+        filteredData = response.data;
+        return filteredData;
+      } else  if (radioSearch === "Unpopular") {
+        const response = await axios.get(`/notices?page=${page}&species=${species}&byPopularity=true`);
+        filteredData = response.data;
+        return filteredData;
+      }
+    } else if (!keyword && !category && !species && locationId && !radioSearch && sex) {
+      const response = await axios.get(`/notices?&limit=2000&locationId=${locationId}`);
+      let filteredResults = response.data.results.filter(item => item.sex === sex);
+      const totalPages = Math.ceil(filteredResults?.length / 6);
+      const displayedObjects = filteredResults.slice(startIndex, endIndex);
+      const sendData = {results: displayedObjects, totalPages: totalPages};
+      return filteredData = sendData;
+    } else if (!keyword && !category && !species && locationId && radioSearch && !sex) {
+      if(radioSearch === "Cheap") {
+        const response = await axios.get(`/notices?page=${page}&locationId=${locationId}&byPrice=true`);
+        filteredData = response.data;
+        return filteredData;
+      } else if (radioSearch === "Expensive") {
+        const response = await axios.get(`/notices?page=${page}&locationId=${locationId}&byPrice=false`);
+        filteredData = response.data;
+        return filteredData;
+      } else if (radioSearch === "Popular") {
+        const response = await axios.get(`/notices?page=${page}&locationId=${locationId}&byPopularity=false`);
+        filteredData = response.data;
+        return filteredData;
+      } else  if (radioSearch === "Unpopular") {
+        const response = await axios.get(`/notices?page=${page}&locationId=${locationId}&byPopularity=true`);
+        filteredData = response.data;
+        return filteredData;
+      }
+    } else if (!keyword && !category && !species && !locationId && radioSearch && sex) {
+      let response;
+      if(radioSearch === "Cheap") {
+        response = await axios.get(`/notices?&limit=2000&byPrice=true`);
+      } else if (radioSearch === "Expensive") {
+        response = await axios.get(`/notices?&limit=2000&byPrice=false`);
+      } else if (radioSearch === "Popular") {
+        response = await axios.get(`/notices?&limit=2000&byPopularity=false`);
+      } else  if (radioSearch === "Unpopular") {
+        response = await axios.get(`/notices?&limit=2000&byPopularity=true`);
+      }
+      let filteredResults = response.data.results.filter(item => item.sex === sex);
+      const totalPages = Math.ceil(filteredResults?.length / 6);
+      const displayedObjects = filteredResults.slice(startIndex, endIndex);
+      const sendData = {results: displayedObjects, totalPages: totalPages};
+      return filteredData = sendData;
+    }
+        // ========================================================================================================================================
+    // ====================  IF ONLY 3 (THREE) PARAMETERS ======================================================================================
     return filteredData;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
