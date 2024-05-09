4b4f1b7e971a3cdc945f2acf09f4cfa6998fc838
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-05-09 10:13:38 +0200

    datepicker

--- src/components/ForAddPetPage/CreationForm/BirthdayInput/BirthdayInput.jsx
+++ src/components/ForAddPetPage/CreationForm/BirthdayInput/BirthdayInput.jsx
@@ -1,26 +1,17 @@
+// @ts-nocheck
 import React from "react";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { TextField } from "@mui/material";
-import { styled } from '@mui/material/styles'
 import IconCalendar from "./IconCalendar";
 
-const StyledTextField = styled(TextField)({
-    color: '#bbdefb',
-    borderRadius: '20px',
-    border: '1px solid var(--link-gray)',
-    backgroundColor: "transparent",
-    outline: "none",
-    height: "42px"
-  })
 
 const BirthdayInput = () => {
+
   return (
     <>
-      <DatePicker 
+      <DatePicker format="DD.MM.YYYY"
         onChange={(newValue) => console.log(newValue)}
         slotProps={{ textField: { size: 'small' } }}
         views={["day", "month", "year"]} slots={{
-            textField: StyledTextField,
             openPickerIcon: IconCalendar,
           }}
       />
