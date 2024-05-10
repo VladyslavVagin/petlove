be60e534a04848a0c975331c426da6c9e637bb05
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-05-10 16:16:29 +0200

    update

--- src/components/ForAddPetPage/CreationForm/BirthdayInput/BirthdayInput.jsx
+++ src/components/ForAddPetPage/CreationForm/BirthdayInput/BirthdayInput.jsx
@@ -3,25 +3,24 @@ import React from "react";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import IconCalendar from "./IconCalendar";
 
-
 const BirthdayInput = () => {
-
   return (
     <>
-      <DatePicker format="DD.MM.YYYY"
+      <DatePicker
+        format="DD.MM.YYYY"
         onChange={(newValue) => console.log(newValue)}
-        slotProps={{ textField: { size: 'small', sx: {
-          ".MuiOutlineInput-root": {border: "none", "& .Mui-focused": {border: "none"}},
-          ".MuiInputBase-root": {border: "none", "& .Mui-focused": {border: "none"}},
-          ".MuiInputBase-input": {border: "none", outline: "none", "&:hover": {border: "none", outline: "none"}},
-        } },  popper: {
-          sx: {
-            ".MuiPaper-root": { border: "1px solid var(--accent-color)"},
+        slotProps={{
+          textField: { size: "small" },
+          popper: {
+            sx: {
+              ".MuiPaper-root": { border: "1px solid var(--accent-color)" },
+            },
           },
-        }, }}
-        views={["day", "month", "year"]} slots={{
-            openPickerIcon: IconCalendar,
-          }}
+        }}
+        views={["day", "month", "year"]}
+        slots={{
+          openPickerIcon: IconCalendar,
+        }}
       />
     </>
   );
